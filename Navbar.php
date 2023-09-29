199f00904fb22e89f0da629ff11c692120a19013
Ralph Hernandez <ralph@Ralphs-MacBook-Air.local>
2023-09-29 20:11:11 +0800

    first commit

---
+++ Navbar.php
@@ -0,0 +1,15 @@
+<nav class="px-16">
+    <ul class="flex justify-between py-5">
+        <div>
+        <h1 class="font-bold"><a href="index.php">Gojo's Recipe</a></h1>
+        </div>
+        <div class="flex space-x-5">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="Menus.php">Menu</a></li>
+        </div>
+        <div>
+        <li><a href="Contact.php">Contact</a></li>
+        </div>
+    </ul>
+</nav>
+
